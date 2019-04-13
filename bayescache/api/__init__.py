696af5b2dacd8eaa35e04f24860c73333463ab38
yngtodd <young.todd.mk@gmail.com>
2019-04-13 15:58:07 -0400

    beefing up api

--- bayescache/api/__init__.py
+++ bayescache/api/__init__.py
@@ -1,5 +1,16 @@
-from .learner import Learner
+from .callback import Callback
 from .info import BatchInfo, EpochInfo, TrainingInfo
+from .learner import Learner
+
 from .model import (
     Model, BackboneModel, LinearBackboneModel, SupervisedModel, RnnLinearBackboneModel, RnnModel, RnnSupervisedModel
 )
+from .model_factory import ModelFactory
+from .optimizer import OptimizerFactory
+from .schedule import Schedule
+from .scheduler import SchedulerFactory
+from .source import Source, TrainingData, TextData
+from .storage import Storage
+from .train_phase import TrainPhase, EmptyTrainPhase
+
+from bayescache.internals.model_config import ModelConfig
---
+++ bayescache/api/optimizer.py
@@ -0,0 +1,9 @@
+from bayescache.api import Model
+from torch.optim import Optimizer
+
+
+class OptimizerFactory:
+    """ Base class for optimizer factories """
+
+    def instantiate(self, model: Model) -> Optimizer:
+        raise NotImplementedError
