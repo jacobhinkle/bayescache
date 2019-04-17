ca16ddc4c3bfc6960b4367cd3bf4fdced13875eb
yngtodd <young.todd.mk@gmail.com>
2019-04-17 12:11:33 -0400

    Adding CNN example

---
+++ bayescache/models/cnn.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from bayescache.metrics.loss_metric import Loss
+from bayescache.metrics.accuracy import Accuracy
+from bayescache.api import SupervisedModel, ModelFactory
+
+
+class Hyperparameters:
+    kernel1 = 5
+    kernel2 = 5 
+
+
+class CNN(SupervisedModel):
+
+    def __init__(self, hparams, num_classes):
+        super(CNN, self).__init__()
+        self.hp = hparams
+        self.conv1 = nn.Conv2d(1, 20, self.hp.kernel1, 1)
+        self.conv2 = nn.Conv2d(20, 50, self.hp.kernel2, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, num_classes)
+
+    def loss_value(self, x_data, y_true, y_pred):
+        """ Calculate a value of loss function """
+        return F.cross_entropy(y_pred, y_true)
+
+    def metrics(self):
+        """ Set of metrics for this model """
+        return [Loss(), Accuracy()]
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+def new(hyperparameters=None, savefile=None):
+    """Create new MTCNN model."""
+    if hyperparameters:
+        hparams = hyperparameters
+    else:
+        hparams = Hyperparameters()
+
+    model = CNN(hparams)
+
+    if savefile:
+        model.load_state_dict(savefile, strict=False)
+
+    return model
+
+
+def create(hyperparameters=None, num_classes=10):
+    """ Vel factory function """
+    def instantiate(**_):
+        if hyperparameters:
+            hparams = hyperparameters
+        else:
+            hparams = Hyperparameters()
+
+        return CNN(hparams, num_classes)
+
+    return ModelFactory.generic(instantiate)
---
+++ bayescache/sources/p3b3.py
@@ -0,0 +1,18 @@
+from bayescache.data.p3b3 import P3B3
+from bayescache.api import TrainingData
+
+
+def create(model_config, batch_size, num_workers=0, augmentations=None):
+    """ Create an Anon dataset."""
+    path = model_config.data_dir('anon')
+
+    train_dataset = P3B3(path, partition='train', download=True)
+    test_dataset = P3B3(path, partition='test', download=True)
+
+    return TrainingData(
+        train_dataset,
+        test_dataset,
+        num_workers=num_workers,
+        batch_size=batch_size,
+        augmentations=augmentations
+    )
